b6afe24d4d8a86a45d4bbd22fcb5396d8f9eec84
Junes El-Sayed <juneselsayed03@hotmail.com>
2025-10-07 22:23:02 +0200

    Added Change

--- src/test/java/com/example/touristguide2/resouces/Touristguide3-mysql.sql
+++ src/test/java/com/example/touristguide2/resouces/Touristguide3-mysql.sql
@@ -1,53 +1,54 @@
--- Den her sleyter gamle tabeller hvis de findes
-DROP TABLE IF EXISTS attraction_tag;
-DROP TABLE IF EXISTS tag;
-DROP TABLE IF EXISTS attraction;
+DROP DATABASE IF EXISTS touristguide3;
+CREATE DATABASE touristguide3;
+USE touristguide3;
 
--- Opretter tables
-CREATE TABLE attraction (
-                            ID INT AUTO_INCREMENT PRIMARY KEY,
-                            NAME VARCHAR(100) NOT NULL,
-                            DESCRIPTION TEXT,
-                            LOCATION VARCHAR(100) NOT NULL
+CREATE TABLE IF NOT EXISTS attraction(
+ID INT PRIMARY KEY AUTO_INCREMENT,
+NAME VARCHAR(100) NOT NULL,
+DESCRIPTION TEXT,
+LOCATION VARCHAR(100) NOT NULL
 );
 
-CREATE TABLE tag (
-                     ID INT AUTO_INCREMENT PRIMARY KEY,
-                     NAME VARCHAR(100) NOT NULL
+
+CREATE TABLE IF NOT EXISTS tag(
+ID INT PRIMARY KEY AUTO_INCREMENT,
+NAME VARCHAR(100) NOT NULL
+
 );
 
-CREATE TABLE attraction_tag (
-                                ATTRACTION_ID INT NOT NULL,
-                                TAG_ID INT NOT NULL,
-                                PRIMARY KEY (ATTRACTION_ID, TAG_ID),
-                                FOREIGN KEY (ATTRACTION_ID) REFERENCES attraction(ID) ON DELETE CASCADE,
-                                FOREIGN KEY (TAG_ID) REFERENCES tag(ID) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS attraction_tag (
+ATTRACTION_ID INT NOT NULL,
+TAG_ID INT NOT NULL,
+PRIMARY KEY (ATTRACTION_ID, TAG_ID),
+FOREIGN KEY (ATTRACTION_ID) REFERENCES attraction(ID) ON DELETE CASCADE,
+FOREIGN KEY (TAG_ID) REFERENCES tag(ID) ON DELETE CASCADE
 );
 
--- Eksempel på data
-INSERT INTO attraction (NAME, LOCATION, DESCRIPTION) VALUES
-                                                         ('Junes', 'Brønshøj', 'Useriøs spiller'),
-                                                         ('Den lille havfrue', 'København', 'En ikonisk statue på Langelinie'),
-                                                         ('Bellasky', 'Amager', 'Et af Skandinaviens flotteste hoteller'),
-                                                         ('Amalienborg', 'København', 'Det danske kongehus residens'),
-                                                         ('SMK', 'København', 'Museum for kunst');
+INSERT INTO attraction(NAME, LOCATION, DESCRIPTION) VALUES
+("Junes", "Brønshøj", "Useriøs spiller"),
+("Den lille havefrue", "København", "En ikonisk statue på langlinje"),
+("Bellasky", "Amager", "EN af Skandinavieske flotteste hoteller"),
+("Amalienborg", "København", "Det danske kongehus residens"),
+("SMK", "København", "Museum for kunst");
 
-INSERT INTO tag (NAME) VALUES
-                           ('Historie'),
-                           ('Kunst'),
-                           ('Arkitektur'),
-                           ('Sport'),
-                           ('Natur'),
-                           ('Museum'),
-                           ('Fodboldspiller');
+INSERT INTO tag(NAME) VALUES
+("Historie"),
+("Kunst"),
+("Arkitektur"),
+("Sport"),
+("Natur"),
+("Museum"),
+("Fodboldspiller");
 
-INSERT INTO attraction_tag (ATTRACTION_ID, TAG_ID) VALUES
-                                                       (1, 4),
-                                                       (1, 7),
-                                                       (2, 1),
-                                                       (2, 3),
-                                                       (3, 3),
-                                                       (4, 1),
-                                                       (4, 3),
-                                                       (5, 2),
-                                                       (5, 6);
+TRUNCATE TABLE attraction_tag;
+
+INSERT INTO attraction_tag(ATTRACTION_ID, TAG_ID) VALUES
+(1, 4),
+(1, 7),
+(2, 1),
+(2, 3),
+(3, 3),
+(4, 1),
+(4, 3),
+(5, 2),
+(5, 6);
